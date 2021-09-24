6ed42dd8a24312b1f48d5ead7371ba78d94f0b5b
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-23 23:36:18 -0500

    fixed middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ console.log('Hello World');
 const absolutePath = __dirname + '/public'
 
 
+app.use(function (req, res, next) {
+  const string = req.method + ' ' + req.path + ' - ' + req.ip
+  console.log(string)
+  next()
+})
+
 const serveString = app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
@@ -16,11 +22,6 @@ app.use(express.static(absolutePath))
 app.use('/public', express.static(absolutePath))
 
 
-app.use(function (req, res, next) {
-  const string = req.method + ' ' + req.path + ' - ' + req.ip
-  console.log(string)
-  next()
-})
 
 app.get('/json', (req, res, next) => {
 
