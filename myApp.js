01f2af9601359f57b15d65075ce67565e9b34fd2
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-23 23:50:30 -0500

    Added now route and chained middleware

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-require('dotenv').config()
 
+require('dotenv').config()
 
 console.log('Hello World');
 const absolutePath = __dirname + '/public'
 
-
+app.use(express.static(absolutePath))
+app.use('/public', express.static(absolutePath))
 app.use(function (req, res, next) {
   const string = req.method + ' ' + req.path + ' - ' + req.ip
   console.log(string)
@@ -17,13 +18,7 @@ const serveString = app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-
-app.use(express.static(absolutePath))
-app.use('/public', express.static(absolutePath))
-
-
-
-app.get('/json', (req, res, next) => {
+const jsonString = app.get('/json', (req, res, next) => {
 
   if (process.env.MESSAGE_STYLE == 'uppercase') {
     res.json(
@@ -36,6 +31,12 @@ app.get('/json', (req, res, next) => {
   }
 })
 
+const newRoute = app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+}, (req, res) => {
+  res.send({ time: req.time })
+})
 
 
 
